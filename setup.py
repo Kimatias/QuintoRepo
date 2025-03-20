568a8605579b91e1e78f605758808332a204c42d
Kimatias <karenxsp@gmail.com>
2025-03-20 17:11:36 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quintoRepo",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Karen Sánchez",                         # Tu nombre
+    author_email="karenxsp@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/kimatias/QuintoRepo",     # URL del proyecto
+)
